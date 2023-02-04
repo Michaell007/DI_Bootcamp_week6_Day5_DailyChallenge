a7a03fe41adbad93b64f0ad50421162d02f46811
Michael Yapi .A <michaelakichi@gmail.com>
2023-02-04 17:40:31 +0000

    Update daily.php

--- daily.php
+++ daily.php
@@ -13,21 +13,19 @@ $incorrectGuesses = 0;
 // initialiser mon mot choisi par "-"
 $hiddenWord = str_repeat("-", strlen($word));
 
-// hmm je vais boucler jusqu'à ce que le mot soit entièrement deviné ou que 5 devinettes incorrectes aient été faites.
-while ($hiddenWord != $word && $incorrectGuesses < 5) {
+// lancement du jeu
+while ($hiddenWord != $word && $incorrectGuesses < 7) {
     echo "Mot actuel: $hiddenWord\n";
     echo "Entrez une lettre: ";
 
-// je vais utiliser la fonction trim(fgets(STDIN))pour lire l'entrée du clavier et supprimer tous les caractères d'espacement du début et de la fin de la chaîne.on sait jamais haha
-
+    // recuperer la saisie du joueur
     $letter = trim(fgets(STDIN));
     if (strpos($word, $letter) === false) {
-
-        // Bon si la lettre n'a pas été trouvé incremente la variable des mauvaises reponses
+        // Si la reponse est incorrecte
         $incorrectGuesses++;
         echo "Incorrect.\n";
     } else {
-        // Ici la lettre est trouvée
+        // Si la reponse est correcte
         for ($i = 0; $i < strlen($word); $i++) {
             if ($word[$i] == $letter) {
                 $hiddenWord[$i] = $letter;
